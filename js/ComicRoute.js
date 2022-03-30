400fdbc86e525dcfd699b884f74666ebffddf197
Seppe Verbeken <seppe.verbeken@student.howest.be>
2022-03-30 14:06:46 +0200

    finished without sort and search

--- js/ComicRoute.js
+++ js/ComicRoute.js
@@ -3,11 +3,22 @@
 export default class ComicRoute{
     constructor(year, photoID, character, author){
         this._year = year;
-        this._photoID = `https://opendata.brussels.be/explore/dataset/comic-book-route/files/${photoID}/300/`;
+        this._photoID = (photoID != null)?`https://opendata.brussels.be/explore/dataset/comic-book-route/files/${photoID}/300/`:photoID;
         this._character = character;
         this._author = author;
     }
     get htmlString() {
-        return;
+        return `<div class="item">
+        <div>
+          <div class="item-field">${this._character}</div>
+          <div class="item-field">${this._author}</div>
+          <div class="item-field">${this._year}</div>
+        </div>
+        
+        <div>
+          <img src="${this._photoID}" />
+        </div>
+        
+      </div> `;
     }
 }
--- js/index.js
+++ js/index.js
@@ -5,17 +5,29 @@ import ComicRoute from './ComicRoute.js';
 const app = {
     items: [],
     init() {
+        document.getElementById("searchForm").addEventListener("submit", function(event){
+            event.preventDefault();
+            app.render();
+        });
         this.getData();
     },
     getData() {
         fetch(`https://opendata.brussels.be/api/records/1.0/search/?dataset=comic-book-route&q=&rows=64`)
         .then(r => r.json())
-        .then(r => r.records.forEach(e => 
-            this.items.push(new ComicRoute(e.fields.annee,e.fields.photo.id,e.fields.personnage_s,e.fields.auteur_s))));
+        .then(r => r.records.forEach(e => {
+            let temp = (e.fields.photo != null)?e.fields.photo.id:null;
+            console.log(temp);
+            
+            this.items.push(new ComicRoute(e.fields.annee,temp,e.fields.personnage_s,e.fields.auteur_s));
+        }));
+
     },
     onSearch() {
     },
     render(message) {
+        this.items.forEach(e =>{
+            document.getElementById("list").innerHTML += e.htmlString;
+        });
     }
 
 };
